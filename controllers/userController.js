a7a5c20f4dc43df9359fa9a5c24c84995156fd0b
BigSev15 <palat008@umn.edu>
2023-01-17 12:46:50 -0600

    made user functions in the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,55 @@
 const User = require('../models/user');
+const Thought = require('../models/thought');
 
 module.exports = {
     getUsers(req, res) {
         User.find()
-          .populate({ path: 'thoughts', select: '-__v' })
-          .select('-__v')
           .then((users) => res.json(users))
           .catch((err) => res.status(500).json(err));
     },
+
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .populate({ path: 'thoughts', select: '-__v' })
+          .populate({ path: 'friends', select: '-__v' })
+          .then((user) =>
+            !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+          .then((user) => res.json(user))
+          .catch((err) => res.status(500).json(err));
+    },
+
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+          .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No user with this id!' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that id!' })
+              : Thought.deleteMany({ _id: { $in: user.thoughts } })
+          )
+          .then(() => res.json({ message: 'User and thoughts deleted!' }))
+          .catch((err) => res.status(500).json(err));
+    },
+
+
 }
